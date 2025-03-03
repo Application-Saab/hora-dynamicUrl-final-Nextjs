6499c817574817bbdb06268cf3ca545c51e81853
GovindaRadha <newuser@example.com>
2025-03-03 17:16:52 +0530

    s

--- horaservices/src/components/pagelayout.jsx
+++ horaservices/src/components/pagelayout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import '../app/globals.css';
-
+import { useRouter } from "next/router";
 
 
 const PageLayout = ({ children }) => {
@@ -12,10 +12,13 @@ const PageLayout = ({ children }) => {
     //     event.preventDefault();
     // });
  
+    const router = useRouter();
+    const { catValue } = router.query;
+    const hideHeader = router.pathname === `/balloon-decoration/[catValue]`;
   
   return (
     <div className="page-container container-fluid p-0">
-      <Header />
+      {!hideHeader && <Header/>}
       <main className="page-main row m-0">
         <section
           // style={{ backgroundColor: getBackgroundColor() }}
