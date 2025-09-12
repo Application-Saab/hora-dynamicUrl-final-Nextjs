ec8375106463340cee3faeb269c5e3ce90a6c671
Shubham Meena <shubhamm.bca2021@ssism.org>
2025-09-12 17:35:21 +0530

    implement get account details API.

--- horaservices/src/pages/accounts/index.js
+++ horaservices/src/pages/accounts/index.js
@@ -1,87 +1,81 @@
-import { BASE_URL, GET_USER_DETAIL_ENDPOINT } from "@/utils/apiconstants";
+import { BASE_URL, GET_USER_BY_ID } from "@/utils/apiconstants";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import "./AccountsPage.css";
 import ArrowIcon from "@/assets/forward_arrow.svg";
 import CallIcon from "@/assets/call_icon.svg";
 import LogoutIcon from "@/assets/logout_icon.svg";
-import Avatar from '@/assets/avtar.jpg';
+import Avatar from "@/assets/avtar.jpg";
 const AccountPage = () => {
   const [userData, setUserData] = useState({});
-  const LocalMobile = localStorage.getItem("mobileNumber");
+  const [errorFetchUser, setErrorFetchUser] = useState(false);
+  const [loadingUser, setLoadingUser] = useState(false);
+  const userId = localStorage.getItem('userID');
+  const token = localStorage.getItem('token')
   const handleLogout = () => {
     localStorage.clear();
-    window.location.href = '/wonderland'
-  }
+    window.location.href = "/wonderland";
+  };
 
-  //   useEffect(() => {
-  //     const fetchEventImages = async () => {
-  //       if (!urlParams.eventId) {
-  //         setErrorEventImages("Event ID not found in URL");
-  //         setLoadingEventImages(false);
-  //         return;
-  //       }
+  useEffect(() => {
+    const fetchEventImages = async () => {
+      if (!userId) {
+        setErrorFetchUser("User id not found ");
+        setLoadingUser(false);
+        return;
+      }
 
-  //       try {
-  //         const response = await fetch(
-  //           `${BASE_URL}${GET_USER_DETAIL_ENDPOINT}/${urlParams?.eventId}`,
-  //           {
-  //             headers: {
-  //               Authorization: `${token}`, // Add token in Authorization header
-  //               "Content-Type": "application/json",
-  //             },
-  //           }
-  //         );
-  //         const data = await response.json();
-  //         if (data.error) {
-  //           setEventAllImages([]);
-  //           setErrorEventImages(data.message || "Failed to fetch guests");
-  //         } else {
-  //           setEventAllImages(data.data || []);
-  //         }
-  //       } catch (err) {
-  //         setErrorEventImages("Error fetching guests: " + err.message);
-  //       } finally {
-  //         setLoadingEventImages(false);
-  //       }
-  //     };
-  //     // Initial call
-  //     fetchEventImages();
-
-  //     // Call every 3 minute
-  //     const interval = setInterval(fetchEventImages, 180000);
-
-  //     // Cleanup interval on unmount
-  //     return () => clearInterval(interval);
-  //   }, [
-  //     urlParams.eventUserId,
-  //     urlParams.eventId,
-  //     urlParams.userType,
-  //     refetchEventImages,
-  //     refetchLuckyDraw,
-  //     refetchLuckyDrawHostDelete,
-  //     refetchLuckyDrawGuestDelete,
-  //   ]);
+      try {
+        const response = await fetch(
+          `${BASE_URL}${GET_USER_BY_ID}/${userId}`,
+          {
+            headers: {
+              Authorization: `${token}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        if (data.error) {
+          setUserData({});
+          setErrorFetchUser(data.message || "Failed to fetch guests");
+        } else {
+          setUserData(data.data || {});
+        }
+      } catch (err) {
+        setErrorFetchUser("Error fetching guests: " + err.message);
+      } finally {
+        setLoadingUser(false);
+      }
+    };
+    // Initial call
+    fetchEventImages();
+  }, [userId]);
 
   return (
+    <>
     <div className="account-ctn">
       <div className="details-ctn">
         <div>
-            <Image src={Avatar} height={100} width={100} className="user-img" />
+          <img src={userData?.avatar ? userData?.avatar : 'https://avatar.iran.liara.run/public/12'} height={100} width={100} className="user-img" />
         </div>
         <div>
-          <p className="account-name">Shubham</p>
+          <p className="account-name">{userData?.name}</p>
         </div>
         <div className="contact-ctn">
           <div className="contact-item">
-           <Image src={CallIcon} height={18} width={18} />
-            <p>91+{LocalMobile}</p>
+            <Image src={CallIcon} height={18} width={18} />
+            <p>{userData?.phone?.includes(91) ? userData?.phone : `+91${userData?.phone}`}</p>
           </div>
           <div>
-          <Image src={ArrowIcon} />
+            <Image src={ArrowIcon} />
           </div>
         </div>
-        <div className="contact-ctn" style={{cursor: 'pointer'}} onClick={handleLogout}>
+        <div
+          className="contact-ctn"
+          style={{ cursor: "pointer" }}
+          onClick={handleLogout}
+        >
           <div className="contact-item">
             <Image src={LogoutIcon} height={18} width={18} />
             <p>Logout</p>
@@ -92,6 +86,8 @@ const AccountPage = () => {
         </div>
       </div>
     </div>
+
+    </>
   );
 };
 
