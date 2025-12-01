b3359279a0cd65a8c938e2c42f7a3f10f979cd6c
Shubham Meena <shubhamm.bca2021@ssism.org>
2025-12-01 20:34:07 +0530

    fix rsvp list naming issue, of non submitted rsvp users

--- horaservices/src/components/wonderland/rsvp/RsvpListModal.jsx
+++ horaservices/src/components/wonderland/rsvp/RsvpListModal.jsx
@@ -15,7 +15,12 @@ const colorsArray = [
   "#6BB266",
 ];
 
-const RsvpListModal = ({ isOpen, onClose, guestData }) => {
+const RsvpListModal = ({ isOpen, onClose, guestData, totalSubmitted }) => {
+  console.log(
+    "%c [ guestData ]-19",
+    "font-size:13px; background:pink; color:#bf2c9f;",
+    guestData
+  );
   if (!isOpen) return null;
   const { width } = useScreenSize();
   return (
@@ -28,54 +33,59 @@ const RsvpListModal = ({ isOpen, onClose, guestData }) => {
       body={
         <>
           <div className="rsvp-list-container w-100">
-            {guestData?.map((item, index) => (
-              <div
-                key={index}
-                className="d-flex justify-content-start align-items-center"
-              >
-                <div
-                  className="d-flex justify-content-center align-items-center list-avatar-ctn"
-                  style={{
-                    backgroundColor: colorsArray[index % colorsArray.length],
-                  }}
-                >
-                  <span>{item.name.charAt(0).toUpperCase()}</span>
-                </div>
-                <div className="d-flex align-items-center justify-content-between list-content-wrapper w-100 gap-2">
+            {guestData?.map(
+              (item, index) =>
+                (item?.rsvpStatus === RSVP_STATUS?.WILL_COME ||
+                  item?.rsvpStatus === RSVP_STATUS?.WILL_TRY) && (
                   <div
-                    className={`${
-                      width > mobileBreakPoints?.small ? "col-7" : "col-6"
-                    } list-names`}
+                    key={index}
+                    className="d-flex justify-content-start align-items-center"
                   >
-                    <span>{item?.name}</span>
+                    <div
+                      className="d-flex justify-content-center align-items-center list-avatar-ctn"
+                      style={{
+                        backgroundColor:
+                          colorsArray[index % colorsArray.length],
+                      }}
+                    >
+                      <span>{item.name.charAt(0).toUpperCase()}</span>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between list-content-wrapper w-100 gap-2">
+                      <div
+                        className={`${
+                          width > mobileBreakPoints?.small ? "col-7" : "col-6"
+                        } list-names`}
+                      >
+                        <span>{item?.name}</span>
+                      </div>
+                      <div
+                        className={`${
+                          width > mobileBreakPoints?.small ? "col-5" : "col-6"
+                        } list-status-icon d-flex justify-content-start align-items-center`}
+                      >
+                        <Image
+                          src={
+                            item?.rsvpStatus === RSVP_STATUS?.WILL_COME
+                              ? WillComeIcon
+                              : WillTryIcon
+                          }
+                          alt="will try"
+                        />
+                        <span>
+                          {item?.rsvpStatus === RSVP_STATUS?.WILL_COME
+                            ? "I’m Going!"
+                            : "Will try"}
+                        </span>
+                      </div>
+                    </div>
                   </div>
-                  <div
-                    className={`${
-                      width > mobileBreakPoints?.small ? "col-5" : "col-6"
-                    } list-status-icon d-flex justify-content-start align-items-center`}
-                  >
-                    <Image
-                      src={
-                        item?.rsvpStatus === RSVP_STATUS?.WILL_COME
-                          ? WillComeIcon
-                          : WillTryIcon
-                      }
-                      alt="will try"
-                    />
-                    <span>
-                      {item?.rsvpStatus === RSVP_STATUS?.WILL_COME
-                        ? "I’m Going!"
-                        : "Will try"}
-                    </span>
-                  </div>
-                </div>
-              </div>
-            ))}
+                )
+            )}
           </div>
-          {guestData?.length > 1 && (
+          {totalSubmitted > 1 && (
             <div className="d-flex w-100 justify-content-center">
               <span className="rsvp-list-bottom-text">
-                {guestData?.length} Total Responses
+                {totalSubmitted} Total Responses
               </span>
             </div>
           )}
