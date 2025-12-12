e66ad8610fceee7fc3789202a34ea208d884d934
BhavnaGurjar <bhavnag340@gmail.com>
2025-12-12 18:49:00 +0530

    fix chaining issue in the order details api

--- horaservices/src/pages/order-details/index.jsx
+++ horaservices/src/pages/order-details/index.jsx
@@ -69,9 +69,9 @@ const OrderDetail = () => {
 
       setOrderDetail(responseData?.data);
       setDecorationItems(responseData?.data?.items[0]?.decoration);
-      setDecorationComments(responseData?.data.decoration_comments);
-      console.log("111" , responseData?.data.add_on);
-      setAddOn(responseData?.data.add_on);
+      setDecorationComments(responseData?.data?.decoration_comments);
+      console.log("111" , responseData?.data?.add_on);
+      setAddOn(responseData?.data?.add_on);
       setLoading(false);
     } catch (error) {
       console.log("fetchDecorationOrderDetails error", error);
@@ -91,9 +91,9 @@ const OrderDetail = () => {
 
       console.log(responseData)
       //console.log(BASE_URL + GET_FOOD_DELIVERY_DETAILS + "/" + orderId)
-      setOrderDetail(responseData.data);
-      setHospitalityServiceCount(responseData.data.no_of_people);
-      setHospitalityServiceTotalAmount(responseData.data.total_amount);
+      setOrderDetail(responseData?.data);
+      setHospitalityServiceCount(responseData?.data?.no_of_people);
+      setHospitalityServiceTotalAmount(responseData?.data?.total_amount);
       setLoading(false);
     } catch (error) {
       console.log("fetchDecorationOrderDetails error", error);
@@ -108,9 +108,9 @@ const OrderDetail = () => {
       );
       const responseData = await response.json();
 
-      setOrderDetail(responseData.data);
-      setHospitalityServiceCount(responseData.data.no_of_people);
-      setHospitalityServiceTotalAmount(responseData.data.total_amount);
+      setOrderDetail(responseData?.data);
+      setHospitalityServiceCount(responseData?.data?.no_of_people);
+      setHospitalityServiceTotalAmount(responseData?.data?.total_amount);
       setLoading(false);
     } catch (error) {
       console.log("fetchOrderDetails error", error);
