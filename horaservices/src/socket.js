dc482dd2e9e850b890f3279092f86eb29050de7c
Shubham Meena <shubhamm.bca2021@ssism.org>
2025-12-16 16:58:27 +0530

    update base url for deployment.

--- horaservices/src/socket.js
+++ horaservices/src/socket.js
@@ -1,10 +1,11 @@
 import { io } from "socket.io-client";
+import { BASE_URL } from "./utils/apiconstants";
 
 let socket;
 
 if (typeof window !== "undefined") {
   const userId = new URLSearchParams(window.location.search).get("id") || localStorage.getItem("userID");
-  socket = io("http://localhost:5000", {
+  socket = io(BASE_URL, {
     transports: ["websocket", "polling"],
     query: { userId }
   });
