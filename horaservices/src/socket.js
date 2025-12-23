4d890c669f3001633218ea438a5ca1fb70d454fc
Shubham Meena <shubhamm.bca2021@ssism.org>
2025-12-23 11:11:15 +0530

    try some chages for socket

--- horaservices/src/socket.js
+++ horaservices/src/socket.js
@@ -2,13 +2,33 @@ import { io } from "socket.io-client";
 import { BASE_URL } from "./utils/apiconstants";
 
 let socket;
+export const connectSocket = (userId) => {
+  if (!userId) return;
+
+  socket = io("https://horaservices.com:3000", {
+    transports: ["websocket", "polling"],
+    secure: true,
+    query: { userId },
+  });
+
+  socket.on("connect", () => {
+    console.log("Socket connected:", socket.id);
+  });
+
+  socket.on("connect_error", (err) => {
+    console.error("Socket error:", err.message);
+  });
+
+  return socket;
+};
 
 if (typeof window !== "undefined") {
-  const userId = new URLSearchParams(window.location.search).get("id") || localStorage.getItem("userID");
-  socket = io(BASE_URL, {
-    transports: ["websocket", "polling"],
-    query: { userId }
-  });
+  const userId =
+    new URLSearchParams(window.location.search).get("id") ||
+    localStorage.getItem("userID");
+  if (userId) {
+    connectSocket(userId);
+  }
 }
 
 export default socket;
