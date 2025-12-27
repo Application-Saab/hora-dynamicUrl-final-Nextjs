65cb442d836e8954528cf75bc9d88f5ac4a6042e
Shubham Meena <shubhamm.bca2021@ssism.org>
2025-12-27 15:07:56 +0530

    Import is not working outside the module, so discard firebase config centralization

--- horaservices/public/firebase-messaging-sw.js
+++ horaservices/public/firebase-messaging-sw.js
@@ -1,14 +1,25 @@
-import { firebaseConfig  } from '@/config/firebaseConfig'
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8FU4YYnwLtRuGiSD5eYpEDIQSpFKh1zI",
+  authDomain: "wonderland-hora.firebaseapp.com",
+  projectId: "wonderland-hora",
+  storageBucket: "wonderland-hora.appspot.com",
+  messagingSenderId: "171662318448",
+  appId: "1:171662318448:web:aa881c252acba6fdd14db5",
+};
+
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload?.notification?.title;
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload?.notification?.body,
+    body: payload.notification.body,
     icon: '/new_logo_light.png'
   };
   
