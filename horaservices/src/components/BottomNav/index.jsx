1c2389b4a0e2a71980ed9545fa868c62b147d8c2
Shubham Meena <shubhamm.bca2021@ssism.org>
2026-01-26 15:07:05 +0530

    change popup button on chat page.

--- horaservices/src/components/BottomNav/index.jsx
+++ horaservices/src/components/BottomNav/index.jsx
@@ -14,9 +14,9 @@ import accountIcon from "../../assets/nav_icon/account.svg";
 import accountIconFill from "@/assets/nav_icon/fillaccount.svg";
 import { useChatStore } from "@/hooks/ChatContext";
 import CustomModal from "../wonderland/common/CustomModal";
-import OtpLogin from "../OtpLoginPopup";
 import "./bottomNav.css";
 import LoginModal from "../wonderland/common/login/LoginModal";
+import CustomButton from "../wonderland/common/CustomButton";
 
 export default function BottomNav() {
   const router = useRouter();
@@ -145,26 +145,24 @@ export default function BottomNav() {
 
             <div className="d-flex justify-content-center">
               {!noChatsPopup && (
-                <button
-                  className="chat-instruction-popup-btn"
+                <CustomButton
+                  title={"Login"}
                   onClick={() => {
                     setShowPopup(false);
                     setShowLoginModal(true);
                   }}
-                >
-                  Login
-                </button>
+                  buttonClass={"chat-instruction-popup-btn"}
+                />
               )}
               {noChatsPopup && (
-                <button
-                  className="chat-instruction-popup-btn"
+                <CustomButton
+                  title={"Go to Invites"}
                   onClick={() => {
                     setShowPopup(false);
                     router.push("/wonderland");
                   }}
-                >
-                  Go to Invites
-                </button>
+                  buttonClass={"chat-instruction-popup-btn"}
+                />
               )}
             </div>
           </div>
@@ -208,7 +206,7 @@ export default function BottomNav() {
         />
       </div>
 
-      <LoginModal 
+      <LoginModal
         isOpen={showLoginModal}
         onClose={() => setShowLoginModal(false)}
       />
