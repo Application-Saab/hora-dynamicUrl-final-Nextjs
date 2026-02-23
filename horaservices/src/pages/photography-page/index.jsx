1fd74f2f984c708bbd7a207f112104e54b822cc9
BhavnaGurjar <bhavnag340@gmail.com>
2026-02-23 11:15:57 +0530

    fix the gallery urls

--- horaservices/src/pages/photography-page/index.jsx
+++ horaservices/src/pages/photography-page/index.jsx
@@ -138,7 +138,7 @@ const brandItems = [
  {poseGridData.map((pose, index) => (
   <a
     key={index}
-    href={`http://localhost:3000/photo-gallery?folderName=${encodeURIComponent(
+    href={`https://horaservices.com/photo-gallery?folderName=${encodeURIComponent(
       pose.folder
     )}&customerId=${pose.customerId}`}
     className="pose-card"
