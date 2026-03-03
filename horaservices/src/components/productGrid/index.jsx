aff0f18165d0ece5fcc0bd900ef456fe66865f74
lakshya jain <lakshyatongya819@gmail.com>
2026-03-03 13:21:02 +0530

    comment-issue-fix

--- horaservices/src/components/productGrid/index.jsx
+++ horaservices/src/components/productGrid/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import logo from "../../assets/new_logo_light.png";
 import "./productGrid.css";
-
+import { COMPRESSED_WEBP_IMG_URL } from "@/utils/apiconstants";
 const ProductGrid = ({ data = [], onCardClick, categoryType  }) => {
   return (
     <div className="decContainer">
@@ -13,21 +13,7 @@ const ProductGrid = ({ data = [], onCardClick, categoryType  }) => {
             className="imageContainer"
             onClick={() => onCardClick?.(item)}
           >
-            {/* <div className="imageWrapper">
-              <Image
-                src={`https://horaservices.com/api/uploads/compressed_webp/${item.featured_image?.split(".")[0]}.webp`}
-                alt={`balloon decoration ${item.name}`}
-                className="decImage"
-                width={300}
-                height={300}
-              />
-              <div className="watermark">
-                <Image src={logo} alt="logo" width={40} height={40} />
-              </div>
-              <div className="discountLabel">
-                ₹ {item.discountDifference.toFixed(0)} off
-              </div>
-            </div> */}
+           
             <div className="imageWrapper">
 
   {/* ✅ Design Type Badge */}
@@ -42,7 +28,7 @@ const ProductGrid = ({ data = [], onCardClick, categoryType  }) => {
     )}
 
   <Image
-    src={`https://horaservices.com/api/uploads/compressed_webp/${item.featured_image?.split(".")[0]}.webp`}
+      src={`${COMPRESSED_WEBP_IMG_URL}${item.featured_image?.split(".")[0]}.webp`}
     alt={`balloon decoration ${item.name}`}
     className="decImage"
     width={300}
