23ff3020c026a1a5f7068998bc8350987474fd12
BhavnaGurjar <bhavnag340@gmail.com>
2026-03-16 14:34:58 +0530

    remove the lakhsy view order details code

--- horaservices/src/pages/order-details/index.jsx
+++ horaservices/src/pages/order-details/index.jsx
@@ -4,8 +4,7 @@ import {
   BASE_URL,
   GET_DECORATION_DETAILS,
   ORDER_DETAILS_ENDPOINT,
-  GET_FOOD_DELIVERY_DETAILS,
-  GET_PHOTOGRAPHY_ORDER_DETAILS
+  GET_FOOD_DELIVERY_DETAILS
 } from "../../utils/apiconstants";
 import { useRouter } from "next/router";
 
@@ -33,12 +32,9 @@ const OrderDetail = () => {
   orderType = parseInt(orderType);
 
   useEffect(() => {
-     if (orderType === 2) {
-    fetchOrderDetailsMenu(); 
-  } 
-  else if (orderType === 8) {
-    fetchPhotographyOrderDetails(); 
-  }  else if (orderType === 1) {
+     if ([2, 8].includes(orderType)) {
+      fetchOrderDetailsMenu();
+    } else if (orderType === 1) {
       fetchDecorationOrderDetails();
     } else if ([6, 7].includes(orderType)) {
       fetchFoodDeliveryOrderDetails();
@@ -52,8 +48,7 @@ const OrderDetail = () => {
     try {
       setLoading(true);
       const response = await fetch(
-  BASE_URL + ORDER_DETAILS_ENDPOINT + "/v1/" + apiOrderId
-      );
+`${BASE_URL}/api/order/order_details_photography/${orderId}`      );
       const responseData = await response.json();
       setOrderDetail(responseData.data);
       setLoading(false);
@@ -61,23 +56,6 @@ const OrderDetail = () => {
       console.log("error", error);
     }
   };
-const fetchPhotographyOrderDetails = async () => {
-  try {
-    setLoading(true);
-
-    const response = await fetch(
-      BASE_URL + GET_PHOTOGRAPHY_ORDER_DETAILS+ "/" + orderId
-    );
-
-    const responseData = await response.json();
-    setOrderDetail(responseData.data);
-
-  } catch (error) {
-    console.log("error", error);
-  } finally {
-    setLoading(false);
-  }
-};
   const fetchDecorationOrderDetails = async () => {
     try {
       setLoading(true);
--- horaservices/src/utils/apiconstants.js
+++ horaservices/src/utils/apiconstants.js
@@ -29,7 +29,6 @@ export const GET_DECORATION_BY_NAME = "/api/Decoration/searchByName/";
 export const GET_FOOD_DELIVERY_DETAILS =
   "/api/order/order_details_food_delivery";
 export const GET_PHOTOGRAPHY_BY_TAG = "/api/photography/searchByTag/"
-export const GET_PHOTOGRAPHY_ORDER_DETAILS = "/api/order/order_details_photography";
 export const IMAGE_UPLOAD = "/api/image_upload";
 
 // Wonderland API Endpoints
