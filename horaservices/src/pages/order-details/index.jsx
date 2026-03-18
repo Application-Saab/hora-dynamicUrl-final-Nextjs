6a6cb89dd7ac5cb987435793562439bd21ef4633
lakshya jain <lakshyatongya819@gmail.com>
2026-03-18 16:54:14 +0530

    chef-order-details-Api-issues

--- horaservices/src/pages/order-details/index.jsx
+++ horaservices/src/pages/order-details/index.jsx
@@ -4,7 +4,8 @@ import {
   BASE_URL,
   GET_DECORATION_DETAILS,
   ORDER_DETAILS_ENDPOINT,
-  GET_FOOD_DELIVERY_DETAILS
+  GET_FOOD_DELIVERY_DETAILS,
+  GET_PHOTOGRAPHY_ORDER_DETAILS
 } from "../../utils/apiconstants";
 import { useRouter } from "next/router";
 
@@ -51,8 +52,8 @@ const OrderDetail = () => {
     try {
       setLoading(true);
       const response = await fetch(
-      `${BASE_URL}/api/order/order_details_photography/${orderId}`      
-    );
+  BASE_URL + ORDER_DETAILS_ENDPOINT + "/v1/" + apiOrderId
+      );
       const responseData = await response.json();
       setOrderDetail(responseData.data);
       setLoading(false);
@@ -60,7 +61,23 @@ const OrderDetail = () => {
       console.log("error", error);
     }
   };
-  
+const fetchPhotographyOrderDetails = async () => {
+  try {
+    setLoading(true);
+
+    const response = await fetch(
+      BASE_URL + GET_PHOTOGRAPHY_ORDER_DETAILS+ "/" + orderId
+    );
+
+    const responseData = await response.json();
+    setOrderDetail(responseData.data);
+
+  } catch (error) {
+    console.log("error", error);
+  } finally {
+    setLoading(false);
+  }
+};
   const fetchDecorationOrderDetails = async () => {
     try {
       setLoading(true);
