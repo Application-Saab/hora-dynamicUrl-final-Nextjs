f094b8f727981225975d8162e5e946ce987ee069
Shubham Meena <shubhamm.bca2021@ssism.org>
2026-04-24 17:42:08 +0530

    fix some production issues.

--- horaservices/src/components/wonderland/event-wall/EventwallGalleryItem.jsx
+++ horaservices/src/components/wonderland/event-wall/EventwallGalleryItem.jsx
@@ -33,4 +33,36 @@ const EventwallGalleryItem = ({ isVideo, indexOnPage, fullVideoSrc, isEventWall=
   );
 };
 
+export const EventwallGalleryItemWonderland = ({ isVideo, thumbnail, indexOnPage }) => {
+  const isLoading = !thumbnail?.postWebpUrl && thumbnail.status !== "done";
+
+  return (
+    <>
+      {isVideo ? (
+          <LazyVideo
+            previewSrc={
+              isLoading ? thumbnail.localPreview : thumbnail.postWebpUrl
+            }
+            fullVideoSrc={thumbnail?.postUrl}
+            isEventWall={true}
+            progress={thumbnail.progress}
+          />
+      ) : (
+        <EventLazyImage
+          key={thumbnail?._id}
+          src={isLoading ? thumbnail.localPreview : thumbnail.postWebpUrl}
+          alt={`Event Image ${indexOnPage + 1}`}
+          progress={thumbnail.progress}
+          wrapperClassName={`event-masonry-item`}
+          postType={thumbnail.postType}
+        />
+      )}
+    </>
+  );
+};
+
+
+
+
+
 export default EventwallGalleryItem;
