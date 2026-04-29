1a457ba37f7ab22872e5a18025967b2f7540d75d
lakshya jain <lakshyatongya819@gmail.com>
2026-04-29 17:30:11 +0530

    change-svg image

--- horaservices/src/components/PhotographyMyOrderEventcapsule/index.jsx
+++ horaservices/src/components/PhotographyMyOrderEventcapsule/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Eventcapsule.css";
-import backgroundImage from "@/assets/EventCapsuleBg.svg";
+import backgroundImage from "@/assets/EventCapsuleBg.webp";
 import Image from "next/image";
 import NoteTcon from "../../../public/new_logo_light.png";
 
