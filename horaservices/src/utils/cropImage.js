6a9b465e816205e883e9ff95bb8822ce2d2e9fe0
Shubham Meena <shubhamm.bca2021@ssism.org>
2026-05-04 17:16:29 +0530

    remove unused code

--- horaservices/src/utils/cropImage.js
+++ horaservices/src/utils/cropImage.js
@@ -1,57 +1,26 @@
-// export default function getCroppedImg(imageSrc, crop) {
-//   return new Promise((resolve) => {
-//     const image = new Image();
-//     image.src = imageSrc;
-//     image.onload = () => {
-//       const canvas = document.createElement("canvas");
-//       canvas.width = crop.width;
-//       canvas.height = crop.height;
-
-//       const ctx = canvas.getContext("2d");
-//       ctx.drawImage(
-//         image,
-//         crop.x,
-//         crop.y,
-//         crop.width,
-//         crop.height,
-//         0,
-//         0,
-//         crop.width,
-//         crop.height
-//       );
-
-//       resolve(canvas.toDataURL("image/jpeg", 0.95));
-//     };
-//   });
-// }
-
-
-
-export default function getCroppedImg(imageSrc, pixelCrop) {
-  return new Promise((resolve, reject) => {
-    const image = new window.Image();
-    image.crossOrigin = "anonymous";
+export default function getCroppedImg(imageSrc, crop) {
+  return new Promise((resolve) => {
+    const image = new Image();
     image.src = imageSrc;
     image.onload = () => {
       const canvas = document.createElement("canvas");
-      canvas.width = pixelCrop.width;
-      canvas.height = pixelCrop.height;
+      canvas.width = crop.width;
+      canvas.height = crop.height;
 
       const ctx = canvas.getContext("2d");
       ctx.drawImage(
         image,
-        pixelCrop.x,
-        pixelCrop.y,
-        pixelCrop.width,
-        pixelCrop.height,
+        crop.x,
+        crop.y,
+        crop.width,
+        crop.height,
         0,
         0,
-        pixelCrop.width,
-        pixelCrop.height
+        crop.width,
+        crop.height
       );
 
       resolve(canvas.toDataURL("image/jpeg", 0.95));
     };
-    image.onerror = reject;
   });
 }
