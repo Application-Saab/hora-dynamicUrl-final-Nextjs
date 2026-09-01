93fbe91f05ddf44bd9f5a174d9cce7dbd4019e57
Lakshya <lakshyatongya819@gmail.com>
2026-09-01 11:25:28 +0530

    points-solve

--- horaservices/src/utils/balloonReviews.js
+++ horaservices/src/utils/balloonReviews.js
@@ -1,76 +1,34 @@
+import image1 from "@/assets/decorationreview/image1.jpg"
+import image2 from "@/assets/decorationreview/image2.jpg"
+import image3 from "@/assets/decorationreview/image3.jpg"
+import image4 from "@/assets/decorationreview/image4.jpg"
+import image5 from "@/assets/decorationreview/image5.jpg"
+import image6 from "@/assets/decorationreview/image6.jpg"
+
 export const balloonreviews = [
   {
-    name: "Harikrishna Thakur",
-    booking: "Booked Decor Service for Party",
-    date: "10 Jan 24",
-    rating: 4,
-    avatar:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa0F4GYEzj2SD3l-ILtMKSL1NArnPj-9UsYA&s",
+    name: "Pooja Sharma",
+    booking: "Booked Decoration for Daughter's 1st Birthday",
+    date: "15 Aug 24",
+    rating: 5,
+    avatar: image1,
     indicators: {
-      Taste: false,
+      Taste: true,
       Behaviour: true,
       Presentation: true,
       Quantity: true,
       Hygiene: true,
       Punctuality: true,
     },
-    text: "Very responsive to communication. Team were very polite in addressing all of my concerns and answering my endless questions in a timely matter.Reasonable prices too! Highly recommend!!",
+    text: "The guy who came for decor did an amazing job. It was my daughter's 1st birthday and I was worried about how it would work because we had already been scammed by some event group before.Everything was perfect.",
   },
 
   {
-    name: "Raju",
-    booking: "Booked party in Delhi",
-    date: "28 Jan 2024",
-    rating: 4,
-    avatar: "https://play-lh.googleusercontent.com/a-/ALV-UjVOQrKBRar3UK4sK6KyxFymcvdJgZR5N12qq_CKezcxylA5icHmIg=s32-rw",
-    indicators: {
-      Taste: true,
-      Behaviour: true,
-      Presentation: true,
-      Quantity: true,
-      Hygiene: true,
-      Punctuality: true,
-    },
-    text: "I was blown away by the creativity and professionalism of Hora Services. The balloon arch they created for our wedding was a showstopper! It added a magical touch to our special day. Truly top-notch service!",
-  },
-  {
-    "name": "Kartik Agarwal",
-    "booking": "Booked Balloon Decoration for Kid's Birthday",
-    "date": "01 July 24",
-    "rating": 4,
-    "avatar": "https://randomuser.me/api/portraits/men/7.jpg",
-    "indicators": {
-      "Taste": false,
-      "Behaviour": true,
-      "Presentation": true,
-      "Quantity": true,
-      "Hygiene": true,
-      "Punctuality": true
-    },
-    "text": "I booked Hora Services for my child’s birthday and they exceeded my expectations! The balloon decorations were colorful, fun, and perfectly themed. The team was punctual, professional, and made everything easy."
-  },
-  {
-    "name": "Shruti Agarwal",
-    "booking": "Booked Decoration for Corporate Event",
-    "date": "29 June 24",
-    "rating": 4,
-    "avatar": "https://randomuser.me/api/portraits/women/12.jpg",
-    "indicators": {
-      "Taste": false,
-      "Behaviour": true,
-      "Presentation": true,
-      "Quantity": true,
-      "Hygiene": true,
-      "Punctuality": true
-    },
-    "text": "Hora Services transformed our corporate event with stunning decor. From floral arrangements to custom branding, everything was flawless. The attention to detail impressed everyone, and I’ll definitely book them again!"
-  },
-  {
-    name: "Aarav Sharma",
-    booking: "Booked Balloon Decoration and Live Catering",
-    date: "10 July 24",
+    name: "Balu Nanduri",
+    booking: "Booked Birthday Decoration for Son",
+    date: "22 Aug 24",
     rating: 5,
-    avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+     avatar: image2,
     indicators: {
       Taste: true,
       Behaviour: true,
@@ -79,40 +37,74 @@ export const balloonreviews = [
       Hygiene: true,
       Punctuality: true,
     },
-    text: "Hora truly exceeded our expectations! The balloon decorations were stunning, and the live catering was a hit with everyone. The team was punctual and very attentive to our needs. Highly recommend their services for any event!",
+    text: "Very nicely decorated. My son was so happy. Thank you to your team for making my son's birthday special.",
   },
+
   {
-    "name": "Vivaan Gupta",
-    "booking": "Booked Balloon Decoration for Engagement",
-    "date": "05 June 24",
-    "rating": 5,
-    "avatar": "https://media.istockphoto.com/id/613557584/photo/portrait-of-a-beautifull-smiling-man.jpg?s=612x612&w=0&k=20&c=hkCg5CrmTKOApePbPOyo1U9GexEfIJOJqoLXJIvcN8E=",
-    "indicators": {
-      "Taste": true,
-      "Behaviour": true,
-      "Presentation": true,
-      "Quantity": true,
-      "Hygiene": true,
-      "Punctuality": true
+    name: "Priya Das",
+    booking: "Booked Decoration Service",
+    date: "05 Sep 24",
+    rating: 5,
+     avatar: image3,
+    indicators: {
+      Taste: true,
+      Behaviour: true,
+      Presentation: true,
+      Quantity: true,
+      Hygiene: true,
+      Punctuality: true,
     },
-    "text": "Absolutely wonderful service! The balloon decorations for our engagement party were perfect, and everything was handled with great professionalism. Couldn’t have asked for a better team to make our day special."
+    text: "The service was exceptionally good and decoration was done as per the image provided. We are extremely happy with it and would certainly recommend them for any decoration requirements.",
   },
+
   {
-    "name": "Rohan Kapoor",
-    "booking": "Booked Balloon Decoration for Baby Shower",
-    "date": "25 April 24",
-    "rating": 3,
-    "avatar": "https://randomuser.me/api/portraits/men/9.jpg",
-    "indicators": {
-      "Taste": false,
-      "Behaviour": true,
-      "Presentation": true,
-      "Quantity": false,
-      "Hygiene": false,
-      "Punctuality": true
+    name: "Diksha Sharma",
+    booking: "Booked Birthday Decoration",
+    date: "18 Sep 24",
+    rating: 5,
+     avatar: image4,
+    indicators: {
+      Taste: true,
+      Behaviour: true,
+      Presentation: true,
+      Quantity: true,
+      Hygiene: true,
+      Punctuality: true,
     },
-    "text": "I hired Hora Services for decorations, and they did an incredible job! The team created the perfect ambiance with their beautiful designs, and I received so many compliments from my guests. Highly recommend!"
+    text: "The decor was completed on time and the team was very professional. They finished their work in time and gave the exact same decor as booked. Totally loved the decor. You should definitely book this!",
+  },
+
+  {
+    name: "Sushmitha Shivakumar",
+    booking: "Booked Decoration Service",
+    date: "02 Oct 24",
+    rating: 5,
+   avatar: image5,
+    indicators: {
+      Taste: true,
+      Behaviour: true,
+      Presentation: true,
+      Quantity: true,
+      Hygiene: true,
+      Punctuality: true,
+    },
+    text: "Amazing services. They have done a spectacular job and delivered the decor as per the image. They also reached the location before time. Really happy with the service!",
+  },
+
+  {
+    name: "Sunil Vishnoi",
+    booking: "Booked Decoration Service",
+    date: "12 Oct 24",
+    rating: 5,
+     avatar: image6,
+    indicators: {
+      Taste: true,
+      Behaviour: true,
+      Presentation: true,
+      Quantity: true,
+      Hygiene: true,
+      Punctuality: true,
+    },
+    text: "I had Adil Shaikh as the service provider from Hora Services, who did a wonderful job. I'm impressed and the decoration was of utmost quality. Adil and his associate were very humble and professional.",
   },
 ];
-
-
