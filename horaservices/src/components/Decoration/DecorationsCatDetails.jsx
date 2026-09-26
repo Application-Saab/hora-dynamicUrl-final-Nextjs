2df511939816e38fd9e2cbafdd88523be8b0ea93
Lakshya <lakshyatongya819@gmail.com>
2026-09-26 17:18:02 +0530

    reviews-chnage

--- horaservices/src/components/Decoration/DecorationsCatDetails.jsx
+++ horaservices/src/components/Decoration/DecorationsCatDetails.jsx
@@ -53,7 +53,7 @@ import { fetchWithError } from "@/utils/fetchWithError";
 import axiosApi from "@/utils/axiosApi";
 import MakeItYoursBanner from "@/components/MakeItYoursBanner";
 import GoogleReviewsCard from "@/components/PhotoGalleryPose/GoogleReviewsCard";
-import { reviewsData } from "@/utils/poselinkreviews";
+import { balloonreviewsproduct } from "@/utils/balloonReviews";
 import ActionButtons from "@/components/Actionbuttons";
 import WhyHoraSection from "@/components/WhyHoraSection";
 
@@ -826,7 +826,7 @@ function DecorationsCatDetails({
             <WhyHoraSection />
 
             <div ref={reviewsRef}>
-              <GoogleReviewsCard reviews={reviewsData} />
+              <GoogleReviewsCard reviews={balloonreviewsproduct} />
             </div>
 
             <BrandBanner
